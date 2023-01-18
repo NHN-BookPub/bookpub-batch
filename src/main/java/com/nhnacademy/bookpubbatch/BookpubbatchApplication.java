7f5b18dbf86bb4415ccafdabb97b409321a86b47
Seohyeon Kim <kgustjgus@naver.com>
2023-01-18 21:54:12 +0900

    init project

---
+++ src/main/java/com/nhnacademy/bookpubbatch/BookpubbatchApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookpubbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookpubbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookpubbatchApplication.class, args);
+	}
+
+}
