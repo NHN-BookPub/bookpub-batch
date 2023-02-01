60934a14aad6e81c4b6aa93aec039c51ddf99254
HoFe-U <hochul-y@naver.com>
2023-02-01 17:07:39 +0900

    [Feat] : 회원에대한 Mapper 추가

---
+++ src/main/java/com/nhnacademy/bookpubbatch/repository/member/MemberMapper.java
@@ -0,0 +1,27 @@
+package com.nhnacademy.bookpubbatch.repository.member;
+
+import com.nhnacademy.bookpubbatch.repository.member.dto.MemberDto;
+import com.nhnacademy.bookpubbatch.repository.member.dto.MemberPromotionDto;
+import java.util.List;
+
+/**
+ * Some description here.
+ *
+ * @author : 유호철
+ * @since : 1.0
+ **/
+public interface MemberMapper {
+    /**
+     * 회원 정보를 가져오는 메서드입니다.
+     *
+     * @return the 회원들이 반환됩니다.
+     */
+    List<MemberDto> getMembers();
+
+    /**
+     * 회원정보를 업데이트하는 메서드입니다.
+     *
+     * @param dto 업데이트할 회원정보가 기입됩니다.
+     */
+    void updateMembers(MemberPromotionDto dto);
+}
