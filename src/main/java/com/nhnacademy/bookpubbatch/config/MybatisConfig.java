b14223fc79de0ea244771b37ae3d9990419f11b7
HoFe-U <hochul-y@naver.com>
2023-02-02 13:47:43 +0900

    [Refactor] : 설정값 변경 + javadoc 추가

--- src/main/java/com/nhnacademy/bookpubbatch/config/MybatisConfig.java
+++ src/main/java/com/nhnacademy/bookpubbatch/config/MybatisConfig.java
@@ -1,13 +1,10 @@
 package com.nhnacademy.bookpubbatch.config;
 
-/**
- * Some description here.
- *
- * @author : 유호철
- * @since : 1.0
- **/
 
+import com.nhnacademy.bookpubbatch.repository.coupon.dto.CouponDto;
 import com.nhnacademy.bookpubbatch.repository.member.dto.MemberDto;
+import com.nhnacademy.bookpubbatch.repository.order.dto.OrderDto;
+import com.nhnacademy.bookpubbatch.repository.order.dto.OrderStateDto;
 import com.nhnacademy.bookpubbatch.repository.tier.dto.TierDto;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
@@ -20,10 +17,24 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import javax.sql.DataSource;
 import java.io.IOException;
 
+
+/**
+ * 마이바티스의 설정값들이 들어있는 config 입니다.
+ *
+ * @author : 유호철
+ * @since : 1.0
+ **/
 @Configuration
 @MapperScan(basePackages = "**.mapper.**", sqlSessionFactoryRef = "sqlSessionFactoryBean")
 public class MybatisConfig {
 
+    /**
+     * 마이바티스에서 SessionFactory 를 생성해주기위한 Bean 입니다.
+     *
+     * @param dataSource shop datasource 기입
+     * @return the sql session factory bean
+     * @throws IOException the io exception
+     */
     @Bean
     public SqlSessionFactoryBean sqlSessionFactoryBean(@Qualifier("shopDb") DataSource dataSource) throws IOException {
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
@@ -33,9 +44,18 @@ public class MybatisConfig {
         return sessionFactory;
     }
 
+    /**
+     * Mybatis 에서 Spring batch 값변환시 serializer error 를 잡아주기위한 Bean 입니다.
+     *
+     * @return the execution context serializer
+     */
     @Bean
     public ExecutionContextSerializer customSerializer() {
-        return new Jackson2ExecutionContextStringSerializer(MemberDto.class.getName(), TierDto.class.getName());
+        return new Jackson2ExecutionContextStringSerializer(MemberDto.class.getName(),
+                TierDto.class.getName(),
+                OrderDto.class.getName(),
+                OrderStateDto.class.getName(),
+                CouponDto.class.getName());
     }
 
 }
