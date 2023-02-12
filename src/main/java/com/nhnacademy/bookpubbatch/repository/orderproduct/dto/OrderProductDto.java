8915cbd27c5bcc0ba958c2759fdddf3c56bccf72
HoFe-U <hochul-y@naver.com>
2023-02-12 20:49:28 +0900

    [Feat] : 주문상품 mapper 연결

---
+++ src/main/java/com/nhnacademy/bookpubbatch/repository/orderproduct/dto/OrderProductDto.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.bookpubbatch.repository.orderproduct.dto;
+
+import lombok.Getter;
+
+/**
+ * 주문상품의 정보가 들어있는 DTO 입니다.
+ *
+ * @author : 유호철
+ * @since : 1.0
+ **/
+@Getter
+public class OrderProductDto {
+    private Long orderProductNo;
+}
