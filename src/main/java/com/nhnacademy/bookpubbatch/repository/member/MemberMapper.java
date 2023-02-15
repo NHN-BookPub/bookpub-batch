5c1c5554db740dea433f96f5b58bc77db2cbad4e
HoFe-U <hochul-y@naver.com>
2023-02-15 19:45:36 +0900

    [Feat] Mybatis 수정

--- src/main/java/com/nhnacademy/bookpubbatch/repository/member/MemberMapper.java
+++ src/main/java/com/nhnacademy/bookpubbatch/repository/member/MemberMapper.java
@@ -2,6 +2,7 @@ package com.nhnacademy.bookpubbatch.repository.member;
 
 import com.nhnacademy.bookpubbatch.repository.member.dto.MemberDto;
 import com.nhnacademy.bookpubbatch.repository.member.dto.MemberPromotionDto;
+import com.nhnacademy.bookpubbatch.repository.order.dto.OrderPointDto;
 import java.util.List;
 
 /**
@@ -24,4 +25,11 @@ public interface MemberMapper {
      * @param dto 업데이트할 회원정보가 기입됩니다.
      */
     void updateMembers(MemberPromotionDto dto);
+
+    /**
+     * 회원의 포인트를 업데이트하는 메서드입니다.
+     *
+     * @param dto the dto
+     */
+    void updateMemberPoint(OrderPointDto dto);
 }
